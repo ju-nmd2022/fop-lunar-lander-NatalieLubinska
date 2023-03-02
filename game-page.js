9df6c4faccfb9ab572d2fc6d4b2f0e9948284893
Natalie Lubinska <luna22id@student.ju.se>
2023-03-02 21:30:00 +0100

    moon and spaceship added

--- game-page.js
+++ game-page.js
@@ -1,4 +1,6 @@
+//stars
 push();
+
 let starX = [];
 let starY = [];
 let starAlpha = [];
@@ -12,8 +14,43 @@ for (let i = 0; i < 400; i++) {
   starY.push(y);
   starAlpha.push(alpha);
 }
+pop();
 
-function draw() {
+function moon() {
+  fill(65, 65, 65);
+  ellipse(400, 700, 700, 600);
+}
+
+function spaceship() {
+  noStroke();
+
+  // spaceship body top
+  fill(255, 0, 0);
+  ellipse(370, 150, 100);
+
+  // spaceship body rec
+  fill(255, 0, 0);
+  rect(320, 150, 100, 200);
+
+  //window circle
+  fill(255, 255, 255);
+  ellipse(370, 200, 60);
+
+  fill(255, 255, 255);
+  rect(340, 200, 60);
+
+  fill(0, 255, 0);
+  ellipse(370, 210, 30);
+}
+
+function startScreen() {
+  background(0, 0, 255);
+  text("Start", 200, 100);
+}
+
+function gameScreen() {
+  background(255, 255, 0);
+
   noStroke();
   background(0, 0, 0);
 
@@ -23,6 +60,33 @@ function draw() {
     starAlpha[index] = starAlpha[index] + 0.02;
   }
 
-  box(100, 00, 100);
+  moon();
+  spaceship();
 }
-pop();
+
+function resultScreen() {
+  background(255, 0, 255);
+  text("Result", 200, 100);
+}
+
+let state = "start";
+
+function draw() {
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+  } else if (state === "result") {
+    resultScreen();
+  }
+}
+
+function mouseClicked() {
+  if (state === "start") {
+    state = "game";
+  } else if (state === "game") {
+    state = "result";
+  } else if (state === "result") {
+    state = "game";
+  }
+}
--- graphics.js
+++ graphics.js
@@ -1,58 +1,30 @@
-let starX = [];
-let starY = [];
-let starAlpha = [];
-
-for (let i = 0; i < 400; i++) {
-  const x = Math.floor(Math.random() * width);
-  const y = Math.floor(Math.random() * height);
-  const alpha = Math.random();
-
-  starX.push(x);
-  starY.push(y);
-  starAlpha.push(alpha);
+function moon() {
+  fill(255, 255, 255);
+  ellipse(400, 700, 700, 600);
 }
 
-function startScreen() {
-  background(0, 0, 255);
-  text("Start", 200, 100);
-}
+moon();
 
-function gameScreen() {
-  background(255, 255, 0);
-
+function spaceship() {
   noStroke();
-  background(0, 0, 0);
 
-  for (let index in starX) {
-    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
-    ellipse(starX[index], starY[index], 3);
-    starAlpha[index] = starAlpha[index] + 0.02;
-  }
+  // spaceship body top
+  fill(255, 0, 0);
+  ellipse(370, 150, 100);
+
+  // spaceship body rec
+  fill(255, 0, 0);
+  rect(320, 150, 100, 200);
+
+  //window circle
+  fill(255, 255, 255);
+  ellipse(370, 200, 60);
+
+  fill(255, 255, 255);
+  rect(340, 200, 60);
+
+  fill(0, 255, 0);
+  ellipse(370, 210, 30);
 }
 
-function resultScreen() {
-  background(255, 0, 255);
-  text("Result", 200, 100);
-}
-
-let state = "start";
-
-function draw() {
-  if (state === "start") {
-    startScreen();
-  } else if (state === "game") {
-    gameScreen();
-  } else if (state === "result") {
-    resultScreen();
-  }
-}
-
-function mouseClicked() {
-  if (state === "start") {
-    state = "game";
-  } else if (state === "game") {
-    state = "result";
-  } else if (state === "result") {
-    state = "game";
-  }
-}
+spaceship();
