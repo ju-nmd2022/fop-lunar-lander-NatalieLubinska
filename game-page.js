7d91a237a554f2c289b5431c4b8323ad3d5b5a55
Natalie Lubinska <luna22id@student.ju.se>
2023-03-04 18:36:56 +0100

    Did so much
    you can now win and lose

--- game-page.js
+++ game-page.js
@@ -1,15 +1,40 @@
+let isGameActive = true;
+let state = "start";
+let spaceshipY = -300;
+let spaceshipX = 100;
+let speed = 1;
+let acceleration = 0.2;
+
+const canvasW = 900;
+const canvasH = 600;
+
+let startBtn;
+let restartBtn;
+
 function setup() {
-  createCanvas(900, 600);
-  window.addEventListener("keydown", moveUp);
+  createCanvas(canvasW, canvasH);
+  frameRate(30);
+  state = "start";
+  isGameActive = true;
+  spaceshipY = -300;
+  spaceshipX = 100;
+  speed = 1;
+  acceleration = 0.2;
+
+  startBtn = createButton("Play");
+  startBtn.mousePressed(playGame);
+  startBtn.style("font-size", "30");
+  startBtn.position(200, 200);
+  startBtn.style("display", "show");
+
+  restartBtn = createButton("Restart");
+  restartBtn.mousePressed(playGame);
+  restartBtn.style("font-size", "30");
+  restartBtn.position(200, 200);
+  restartBtn.style("display", "none");
 }
 
-function moveUp(event) {
-  if (event.code === "ArrowUp") {
-    spaceshipY -= 150; // Move the spaceship
-  }
-}
-
-//stars
+//star
 push();
 
 let starX = [];
@@ -17,8 +42,8 @@ let starY = [];
 let starAlpha = [];
 
 for (let i = 0; i < 400; i++) {
-  const x = Math.floor(Math.random() * width);
-  const y = Math.floor(Math.random() * height);
+  const x = Math.floor(Math.random() * canvasW);
+  const y = Math.floor(Math.random() * canvasH);
   const alpha = Math.random();
 
   starX.push(x);
@@ -35,6 +60,7 @@ function moon() {
 function spaceship(x, y) {
   noStroke();
   translate(x, y);
+
   // spaceship body top
   fill(255, 0, 0);
   ellipse(355, 150, 50);
@@ -52,15 +78,27 @@ function spaceship(x, y) {
 }
 
 function startScreen() {
-  background(0, 0, 255);
-  text("Start", 200, 100);
-}
+  background(0, 0, 0);
 
-let spaceshipY = -300;
-let velocity = 0.3;
-let acceleration = 0.1;
-let isGameActive = true;
-acceleration = 0.1;
+  push();
+  fill(255, 255, 255);
+  textSize(20);
+  text("Click to start", 300, 300);
+  text("Use up arrow key to  to land safely", 200, 100);
+
+  pop();
+
+  if (keyIsDown(UP_ARROW)) {
+    state = "game";
+  }
+
+  //stars for background
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 3);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+}
 
 function gameScreen() {
   background(255, 255, 0);
@@ -74,38 +112,70 @@ function gameScreen() {
   }
 
   moon();
-  spaceship(100, spaceshipY);
+  spaceship(spaceshipX, spaceshipY, 0.4, keyIsDown(38));
 
-  spaceshipY = spaceshipY + velocity;
-  velocity = velocity + acceleration;
+  spaceshipY = spaceshipY + speed;
+  speed = speed + acceleration;
 
-  if (spaceshipY >= 200) {
-    velocity = 0;
-  } else {
-    spaceshipY = spaceshipY + velocity;
-    velocity = velocity + acceleration;
+  if (keyIsDown(38)) {
+    speed = speed - 0.5;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    spaceshipX -= 4;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    spaceshipX += 4;
+  }
+
+  if (spaceshipY >= 200 && speed > 10) {
+    speed = 1;
+    acceleration = 0.2;
+    state = "lose";
+    //spaceshipY = 100;
+  } else if (spaceshipY >= 200) {
+    speed = 1;
+    acceleration = 0.2;
+    state = "win";
+    spaceshipY = 100;
   }
 }
 
-function resultScreen() {
+function winScreen() {
   background(255, 0, 255);
-  text("Result", 200, 100);
+  text("Win", 200, 100);
+  restartBtn.style("display", "show");
 }
 
-let state = "start";
+function looseScreen() {
+  background(255, 0, 255);
+  fill(255, 255, 255);
+  textSize(20);
+  text("Loose", 200, 100);
+  if (keyIsDown(BACKSPACE)) {
+    setup();
+  }
+}
 
 function draw() {
+  clear();
   if (state === "start") {
     startScreen();
   } else if (state === "game") {
     gameScreen();
-  } else if (state === "result") {
-    resultScreen();
+  } else if (state === "win") {
+    winScreen();
+  } else if (state === "lose") {
+    looseScreen();
   }
 }
 
-function mouseClicked() {
-  if (state === "start") {
-    state = "game";
-  }
+function playGame() {
+  startBtn.style("display", "none");
+  restartBtn.style("display", "none");
+  state = "game";
+  isGameActive = true;
+  spaceshipY = -300;
+  spaceshipX = 100;
+  speed = 1;
+  acceleration = 0.2;
 }
